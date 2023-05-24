4cc69ba9dd4bfe3ead7ab0c84f4c58d1cc5e4150
unknown <117041148+amNotAWalnutToday@users.noreply.github.com>
2023-05-25 00:29:36 +0100

    Setup router

---
+++ src/RouteSwitch.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+
+export default function RouteSwitch() {
+    return (
+        <Router>
+            <Routes>
+                <Route 
+                    path='/'
+                    element={
+                        <Homepage
+                            
+                        />
+                    }
+                />
+            </Routes>
+        </Router>
+    )
+}
